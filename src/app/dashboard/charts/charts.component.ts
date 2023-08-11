df9ecd99bf71ee19e0a3a7e2c5962887d270b909
Hanani Balqis <hananibalqis1@gmail.com>
2023-08-12 01:29:57 +0800

    Add pie and bar chart using amchart4

--- src/app/dashboard/charts/charts.component.ts
+++ src/app/dashboard/charts/charts.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
 
 @Component({
   selector: 'app-charts',
   templateUrl: './charts.component.html',
   styleUrls: ['./charts.component.scss']
 })
-export class ChartsComponent {
+export class ChartsComponent implements OnInit {
+
+  @Output() pieChart = new EventEmitter<any>();
+  @Output() barChart = new EventEmitter<any>();
+
+  constructor(){}
+  ngOnInit(): void {
+    // this.tableFirst();
+  }
+
+  tableFirst(){
+    let chart = am4core.create("chartdiv", am4charts.PieChart);
+
+    chart.data = [{
+      "country": "Lithuania",
+      "litres": 10
+    }, {
+      "country": "Czech Republic",
+      "litres": 20
+    }, {
+      "country": "Ireland",
+      "litres": 50
+    }, {
+      "country": "Germany",
+      "litres": 20
+    }];
+
+    chart.innerRadius = am4core.percent(40);
+
+    var pieSeries = chart.series.push(new am4charts.PieSeries());
+    pieSeries.dataFields.value = "litres";
+    pieSeries.dataFields.category = "country";
+
+    this.pieChart.emit(chart);
+  }
+
+  tableSecond(){
+    let chart = am4core.create("chartdiv", am4charts.XYChart);
+
+    chart.data = [{
+      "country": "Lithuania",
+      "litres": 50
+    }, {
+      "country": "Czechia",
+      "litres": 100
+    }, {
+      "country": "Ireland",
+      "litres": 90
+    }, {
+      "country": "Germany",
+      "litres": 40
+    }, {
+      "country": "Australia",
+      "litres": 70
+    }, {
+      "country": "Austria",
+      "litres": 10
+    }, {
+      "country": "UK",
+      "litres": 95
+    }];
+
+    let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
+    this.barChart.emit(chart);
+  }
 
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+  charts = ['pieChart', 'barChart'];
+
+  chart1: any;
+  chart2: any;
+
+  constructor(){}
+
+  ngOnInit(): void {
+  }
+
+  pieChart(pie: any){
+    this.chart1 = pie;
+    console.log("received pie chart");
+  }
+
+  barChart(bar: any){
+    this.chart2 = bar;
+    console.log("received bar chart");
+  }
 
 }
